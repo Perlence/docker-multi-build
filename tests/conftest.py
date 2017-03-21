54fd91b4c6bb326503cba5498e225c4ea38398ec
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2017-03-21 23:27:04 +0300

    Reimplement 'isolated_filesystem' fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,7 @@
-from click.testing import CliRunner
 import pytest
 
 
 @pytest.fixture
-def isolated_filesystem():
-    runner = CliRunner()
-    with runner.isolated_filesystem() as fs:
-        yield fs
+def isolated_filesystem(tmpdir):
+    with tmpdir.as_cwd():
+        yield tmpdir
